9b7135482cdd9d987f7c082d019265525df137d7
MattUreal <matt221219@gmail.com>
2023-05-17 00:05:09 +0300

    nn

--- WebApplication4/Models/Registration.cs
+++ WebApplication4/Models/Registration.cs
@@ -2,5 +2,10 @@
 {
     public class Registration
     {
+        public int SId { get; set; }
+        public string? C_Code { get; set; }
+        public DateTime Reg_Date { get; set;}
+        public Double Fee { get; set; }
+        public string? Term { get; set; }
     }
 }
--- WebApplication4/Models/Student.cs
+++ WebApplication4/Models/Student.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApplication4.Models
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace WebApplication4.Models
 {
     public class Student
     {
+        public int Id { get; set; }
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? Sex { get; set; }
+        public string? Batch { get; set; }
+        public int ApprovalFlag { get; set; }
+        
+
     }
 }
